e0f674d8e195748d3675267e27065e6c1d6bb23e
Yaroslav337 <pelerovteatr@gmail.com>
2023-10-11 01:56:51 +0300

    completed hw02-express

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
 
-const contactsRouter = require('./routes/api/contacts')
+const logger = require("morgan");
 
-const app = express()
+const cors = require("cors");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const { contactsRouter } = require("./routes/api");
 
-app.use('/api/contacts', contactsRouter)
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+app.use("/api/contacts", contactsRouter);
 
-module.exports = app
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _, res, __) => {
+  let { status = 500, message } = err;
+
+  if (message.includes("ENOENT")) {
+    message = "Server error";
+  }
+
+  res.status(status).json({ message });
+});
+
+module.exports = app;
